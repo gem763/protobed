0d164171468e5fdfeaba5fdfa0d8f1282698f9a6
sejinkang <gem763@hanmail.net>
2019-08-20 02:46:24 +0900

    .

--- data/urls.py
+++ data/urls.py
@@ -4,4 +4,5 @@ from . import views as v
 urlpatterns = [
     # path('', v.intro, name='intro'),
     path('newscloud/', v.newscloud, name='newscloud'),
+    path('update_newscloud/', v.update_newscloud, name='update_newscloud'),
 ]
