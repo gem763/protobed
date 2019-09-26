70eaef742d60a1e3958494bd22849e221704f666
gem763 <16988092+gem763@users.noreply.github.com>
2019-09-26 17:00:30 +0900

    .

---
+++ etc/news_publishers.py
@@ -0,0 +1,42 @@
+src = {
+    'huffpost':         {'domain':'https://huffpost.com',              'pubname':'HuffPost'}, 
+    'cnn':              {'domain':'https://cnn.com',                   'pubname':'CNN'}, # ?=
+    'investingcom':     {'domain':'https://investing.com',             'pubname':'Investing.com'}, 
+    'politico':         {'domain':'https://politico.com',              'pubname':'POLITICO'}, 
+    'time':             {'domain':'https://time.com',                  'pubname':'TIME'}, 
+    'cnbc':             {'domain':'https://cnbc.com',                  'pubname':'CNBC'}, 
+    'foxnews':          {'domain':'https://foxnews.com',               'pubname':'FOX News'}, 
+    'foxbusiness':      {'domain':'https://foxbusiness.com',           'pubname':'FOX Business'}, 
+    'bbc':              {'domain':'https://bbc.com',                   'pubname':'BBC'}, 
+    'businessinsider':  {'domain':'https://businessinsider.com',       'pubname':'Business Insider'}, 
+    'morningstar':      {'domain':'https://morningstar.com',           'pubname':'Morningstar'}, 
+    'wsj':              {'domain':'https://wsj.com',                   'pubname':'Wall Street Journal'}, # ?mod=rsswn
+    'nyt':              {'domain':'https://nytimes.com',               'pubname':'NewYork Times'}, 
+    'guardian':         {'domain':'https://theguardian.com',           'pubname':'Guardian'}, 
+    'reuters':          {'domain':'https://reuters.com',               'pubname':'Reuters'}, # ?=불필요, 심각
+    'washingtontimes':  {'domain':'https://washingtontimes.com',       'pubname':'Washington Times'}, 
+    'washingtonpost':   {'domain':'https://washingtonpost.com',        'pubname':'Washington Post'}, # ?=
+    'cbs':              {'domain':'https://cbsnews.com',               'pubname':'CBS'}, 
+    'marketwatch':      {'domain':'https://marketwatch.com',           'pubname':'MarketWatch'}, # ?= 심각
+    'atlantic':         {'domain':'https://theatlantic.com',           'pubname':'Atlantic'}, 
+    'vice':             {'domain':'https://vice.com',                  'pubname':'VICE'}, 
+    'npr':              {'domain':'https://npr.org',                   'pubname':'npr'}, 
+    'newrepublic':      {'domain':'https://newrepublic.com',           'pubname':'NEW REPUBLIC'}, 
+    'yahoo':            {'domain':'https://yahoo.com',                 'pubname':'yahoo'}, 
+    'independent':      {'domain':'https://independent.co.uk',         'pubname':'INDEPENDENT'}, 
+    'heritage':         {'domain':'https://heritage.org',              'pubname':'Heritage'}, 
+    'zdnet':            {'domain':'https://www.zdnet.com',             'pubname':'ZDNet'}, # 반드시 www가 붙어야함
+    'townhall':         {'domain':'https://townhall.com',              'pubname':'Townhall'}, 
+    'abcnews':          {'domain':'https://abcnews.go.com',            'pubname':'ABC News'}, # ?=많긴한데 반드시필요
+    'hotair':           {'domain':'https://hotair.com',                'pubname':'HOT AIR'}, 
+    'cbc':              {'domain':'https://cbc.ca',                    'pubname':'CBC'}, 
+    'nymag':            {'domain':'https://nymag.com',                 'pubname':'NewYork Magazine'}, 
+    'thestreet':        {'domain':'https://www.thestreet.com',         'pubname':'TheStreet'}, # 반드시 www가 붙어야함 
+    'thinkprogress':    {'domain':'https://thinkprogress.org',         'pubname':'ThinkProgress'}, 
+    'dailybeast':       {'domain':'https://thedailybeast.com',         'pubname':'DAILY BEAST'}, 
+    'realclearpolitics':{'domain':'https://www.realclearpolitics.com', 'pubname':'RealClear Politics'}, # 반드시 www가 붙어야함
+    #'forbes':          'https://forbes.com', 
+    #'hbr':             'https://hbr.org', 
+    #'ft':              'https://ft.com', 
+    #'economist':       'https://economist.com', 
+}
